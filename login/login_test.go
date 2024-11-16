f52ae82e5702c913ad10f5dfd1c81321fa5a5f5e
halprin <halprin@users.noreply.github.com>
2024-11-15 18:36:00 -0700

    Add another test for getMfaCodeUnlessSpecified

--- login/login_test.go
+++ login/login_test.go
@@ -2,6 +2,7 @@ package login
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -23,10 +24,35 @@ func Test_setSensibleDefaults_profileAndDurationSpecified(t *testing.T) {
 }
 
 func Test_getMfaCodeUnlessSpecified_returnsSameCodeAsProvided(t *testing.T) {
-	inputMfaCode := "DogCow"
+	inputMfaCode := "266226"
 
 	outputMfaCode, err := getMfaCodeUnlessSpecified(inputMfaCode)
 
 	assert.NoError(t, err)
 	assert.Equal(t, inputMfaCode, outputMfaCode)
+}
+
+func Test_getMfaCodeUnlessSpecified_returnsCodeFromStdin(t *testing.T) {
+	stdinMfaCode := "266226"
+
+	readPipe, writePipe, err := os.Pipe()
+	assert.NoError(t, err)
+
+	defer readPipe.Close()
+	defer writePipe.Close()
+
+	originalStdin := os.Stdin
+	defer func() {
+		// reset STDIN back to real STDIN
+		os.Stdin = originalStdin
+	}()
+	os.Stdin = readPipe
+
+	_, err = writePipe.WriteString(stdinMfaCode + "\n")
+	assert.NoError(t, err)
+
+	outputMfaCode, err := getMfaCodeUnlessSpecified("")
+
+	assert.NoError(t, err)
+	assert.Equal(t, stdinMfaCode, outputMfaCode)
 }
